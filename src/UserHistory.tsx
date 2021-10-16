fa7c3e03a4030d58f841378d0ce11696f1f55260
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2021-10-16 16:41:22 +0900

    Draw charts that visualizes past results

--- src/UserHistory.tsx
+++ src/UserHistory.tsx
@@ -3,6 +3,8 @@ import { RouteComponentProps } from "react-router-dom";
 import { apiEndpoint } from "./common";
 import { characters } from "./characters";
 import { FormattedDate } from "./CommonComponents";
+import HighchartsReact from "highcharts-react-official";
+import Highcharts from "highcharts";
 
 interface RankingEntryApi {
   retrieve_id: string;
@@ -22,20 +24,26 @@ interface RankingEntry {
   score: number;
 }
 
+interface RouteProps {
+  eventId: string;
+  characterId: string;
+  earthUserId: string;
+}
+
+interface State {
+  history: null | RankingEntryApi[];
+  chartExtremes: { min: null | number; max: null | number };
+}
+
 export class UserHistoryRoot extends React.Component<
-  RouteComponentProps<{
-    eventId: string;
-    characterId: string;
-    earthUserId: string;
-  }>,
-  {
-    history: null | RankingEntryApi[];
-  }
+  RouteComponentProps<RouteProps>,
+  State
 > {
   constructor(props: any) {
     super(props);
     this.state = {
       history: null,
+      chartExtremes: { min: null, max: null },
     };
   }
 
@@ -49,14 +57,64 @@ export class UserHistoryRoot extends React.Component<
   }
 
   render() {
-    const nicknamme =
-      (this.state.history || []).slice(-1)[0]?.nickname || "???";
+    const nickname = (this.state.history || []).slice(-1)[0]?.nickname || "???";
+    const scoreData = this.state.history?.map((e) => [
+      +new Date(e.retrieve_start),
+      e.score,
+    ]);
+    const rankData = this.state.history?.map((e) => [
+      +new Date(e.retrieve_start),
+      e.rank,
+    ]);
+    // const afterSetExtremes: Highcharts.AxisSetExtremesEventCallbackFunction = (
+    //   event
+    // ) => {
+    //   console.log(event);
+    //   this.setState({ chartExtremes: { min: event.min, max: event.max } });
+    // };
     return (
       <>
         <h3>
-          {nicknamme} さんの {characters[this.props.match.params.characterId]}{" "}
+          {nickname} さんの {characters[this.props.match.params.characterId]}{" "}
           の履歴
         </h3>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={{
+            title: {
+              text: "ファン人数の推移",
+            },
+            series: [{ data: scoreData, name: nickname }],
+            xAxis: {
+              type: "datetime",
+              gridLineWidth: 1,
+              // ...this.state.chartExtremes,
+              // events: { afterSetExtremes },
+            },
+            yAxis: { title: { text: "ファン人数" } },
+            chart: { zoomType: "x" },
+          }}
+        />
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={{
+            title: {
+              text: "順位の推移",
+            },
+            series: [{ data: rankData, name: nickname }],
+            xAxis: {
+              type: "datetime",
+              gridLineWidth: 1,
+              // ...this.state.chartExtremes,
+              // events: { afterSetExtremes },
+            },
+            yAxis: {
+              title: { text: "順位" },
+              reversed: true,
+            },
+            chart: { zoomType: "x" },
+          }}
+        />
         {this.state.history ? (
           <table>
             <thead>
